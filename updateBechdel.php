8a45a542662cf3442cd99ed28c0bb0b28ebdd823
Alexander Schulze <github@lxhome.de>
2026-08-15 19:39:24 +0200

    korrektes Update-Script für Bechdeltest-Daten

--- updateBechdel.php
+++ updateBechdel.php
@@ -7,27 +7,40 @@ if ( PHP_SAPI == 'cli' )
 
 function updateBechdelCache()
 {
-    $api_url = 'bechdel.html';
+    $api_url = 'https://bechdeltest.com/';
+
+    $page = 0;
 
     $entries = [];
 
-    //$opts = stream_context_create ( [ 'http' => [ 'timeout' => 15 ]] );
+    $opts = stream_context_create ( [ 'http' => [ 'timeout' => 15 ]] );
 
-    if ( false !== ( $res = file_get_contents ( $api_url, false /*, $opts*/ ) ) )
+    while ( true )
     {
-        preg_match_all ( '~<div class="movie">(.*)</div>~Us', $res, $movies );
+        $url = ( $page > 0 ) ? $api_url . '/page/' . $page .'/' : $api_url;
 
-        foreach ( $movies [ 1 ] as $movie )
+        if ( false !== ( $res = file_get_contents ( $url, false, $opts ) ) )
         {
-            if ( preg_match ( '~^<a href="https://www.imdb.com/title/(?:tt)?([0-9]+)/".*alt="\[\[([0-3])]]".*href="/view/([0-9]+)/[^>]+>(.+)</a>~Us', $movie, $matches ) )
+            preg_match_all ( '~<div class="movie">(.*)</div>~Us', $res, $movies );
+
+            foreach ( $movies [ 1 ] as $movie )
             {
-                $entries[] = [
-                    '@imdb_id'        => intval ( $matches [ 1 ] ),
-                    '@bechdel_id'     => $matches [ 3 ],
-                    '@bechdel_rating' => $matches [ 2 ],
-                    '$title'          => $matches [ 4 ]
-                ];
+                //                                                             IMDB-ID            RATING                 BECHDEL-ID     TITLE
+                if ( preg_match ( '~<a href="https://www.imdb.com/title/(?:tt)?([0-9]+)/".*alt="\[([0-3])]".*href="/view/([0-9]+)/[^>]+>(.+)</a>~Us', $movie, $matches ) )
+                {
+                    $entries[] = [
+                        '@imdb_id'        => intval ( $matches [ 1 ] ),
+                        '@bechdel_id'     => $matches [ 3 ],
+                        '@bechdel_rating' => $matches [ 2 ],
+                        '$title'          => $matches [ 4 ]
+                    ];
+                }
             }
+
+            $page++;
+
+            if ( !preg_match ( '~<nav class="pager".*<a href="/page/'.$page.'/~Us', $res ) )
+                break;
         }
     }
 
